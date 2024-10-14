6e40cb3a5e4abc6604a381ff8e8872cf0be5285c
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2024-10-14 13:52:42 -0300

    Update Aboult.jsx

--- src/sections/About/Aboult.jsx
+++ src/sections/About/Aboult.jsx
@@ -9,7 +9,7 @@ function About() {
         <img src={profileImg} alt="Larissa Albuquerque" className={styles.profileImg} />
         <div className={styles.aboutText}>
         <p>
-        Olá, sou Larissa, desenvolvedora Full Stack e mestranda em Ciência da Computação no CIN-UFPE. Com foco em Engenharia de Software, minha pesquisa busca aprimorar a avaliação da maturidade de projetos usando técnicas de IA. Tenho experiência em TypeScript, JavaScript, NextJS, ReactJS, Java, SpringBoot, C# e .NET, criando soluções inovadoras que unem tecnologia e design.
+        Olá, sou Larissa, desenvolvedora Full Stack e mestranda em Ciência da Computação no CIN-UFPE. Com foco em Engenharia de Software, minha pesquisa busca aprimorar a avaliação da maturidade de projetos usando técnicas de IA. Tenho experiência em TypeScript, JavaScript, NextJS, ReactJS, Python e Django, criando soluções inovadoras que unem tecnologia e design.
         </p>
 
         </div>
